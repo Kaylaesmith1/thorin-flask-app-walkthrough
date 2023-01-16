dd92c3140601606a905f9fab6c014ac6ed7a8137
Kayla S <kaylaesmith1@gmail.com>
2023-01-16 18:41:26 +0000

    Install Bootstrap, link pages ththrough base.html.

--- run.py
+++ run.py
@@ -8,8 +8,24 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-    if __name__ == "__main__":
-        app.run(
-            host=os.environ.get("IP", "0.0.0"),
-            port=int(os.environ.get("PORT", "5000")),
-            debug=True)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
